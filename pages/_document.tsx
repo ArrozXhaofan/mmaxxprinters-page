8f9930846fc5aa337a90af7e38bef7272f34e85e
ArrozXhaofan <jean.ganador12@gmail.com>
2023-05-18 09:53:20 -0500

    v1 with icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head title='MMAXX Printers'>
+        <link href='maxIcon.png' rel='icon' />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
