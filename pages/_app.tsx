fc072f7357089b2fdd28bd4917d5214248b8322a
ArrozXhaofan <jean.ganador12@gmail.com>
2023-05-18 10:16:27 -0500

    titulo y icono

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from './layout'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return  <Layout><Component {...pageProps} /></Layout> 
+  return ( 
+    <>
+      <Head>
+        <title>MMAXX Printers</title>
+        <link rel="icon" href="maxIcon.png" />
+      </Head>
+     <Layout><Component {...pageProps} /></Layout> 
+    </>
+   
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head title='MMAXX Printers'>
-        <title>MMAXX Printers</title>
-        <link href='maxIcon.png' rel='icon' />
-      </Head>
-
+    <Html lang="es">
+      <Head title='MMAXX Printers'/>
       <body>
         <Main />
         <NextScript />
