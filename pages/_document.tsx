850d1448b52d6ddecdf5a73b8c3f74062c4cfb00
ArrozXhaofan <jean.ganador12@gmail.com>
2023-05-18 10:07:31 -0500

    t1

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head title='MMAXX Printers'>
+        <title>MMAXX Printers</title>
         <link href='maxIcon.png' rel='icon' />
       </Head>
 
